c18c4e5894d8f0dd3ea5a0028ed9524abfd14dc0
Bullobis <2671369836@qq.com>
2026-06-12 18:30:56 +0800

    Initial project setup: root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    kotlin("android") version "1.9.20" apply false
+    kotlin("jvm") version "1.9.20" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
